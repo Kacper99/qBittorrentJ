c29787530002944fbaae765600e279ce58f460f4
Kacper Martela <kacper99@hotmail.co.uk>
2023-07-24 19:28:37 +0100

    docs: qBittorrent test container documentation

--- qBittorrentJ/src/integrationTest/java/io/github/kacper99/qbittorrentj/testcontainers/QBittorrentTestContainer.java
+++ qBittorrentJ/src/integrationTest/java/io/github/kacper99/qbittorrentj/testcontainers/QBittorrentTestContainer.java
@@ -4,6 +4,15 @@ import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+/**
+ * Spins up a test container containing LinuxServer.io qBittorrent container.
+ * <p>
+ * As the test container uses a random port on the host machine, a config is provided in integration test resources
+ * which disables host header validation which is then mounted to the test container
+ *
+ * @see <a href="https://java.testcontainers.org/">Testcontainers</a>
+ * @see <a href="https://hub.docker.com/r/linuxserver/qbittorrent">qBittorrent container</a>
+ */
 public class QBittorrentTestContainer extends GenericContainer<QBittorrentTestContainer> {
 
     private static final DockerImageName IMAGE_NAME = DockerImageName.parse("lscr.io/linuxserver/qbittorrent:latest");
